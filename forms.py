df564b0b071af3fca7b6dcb62bab636c587e1275
meesamhydri <meesamhydri@gmail.com>
2025-08-22 20:26:58 +0500

    Update Boolean to RadioField for storing the correct values

--- forms.py
+++ forms.py
@@ -1,16 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField, SelectField, EmailField
-from wtforms.validators import DataRequired, Email, Length, URL
+from wtforms.validators import DataRequired, Email, Length, URL, InputRequired
+
 
 class AddCafeForm(FlaskForm):
     name = StringField("Cafe Name", validators=[DataRequired()])
     map_url = StringField("Google Maps URL", validators=[DataRequired(), URL()])
     img_url = StringField("Image URL", validators=[DataRequired(), URL()])
     location = StringField("Location", validators=[DataRequired()])
-    has_sockets = BooleanField("Has Sockets?")
-    has_toilet = BooleanField("Has Toilet?")
-    has_wifi = BooleanField("Has WiFi?")
-    can_take_calls = BooleanField("Can Take Calls?")
+    has_sockets = RadioField(
+        "Has sockets?",
+        choices=[("yes","Yes"),("no", "No")],
+        validators=[InputRequired()]
+    )
+    has_toilet = RadioField(
+        "Has toilet?",
+        choices=[("yes","Yes"),("no", "No")],
+        validators=[InputRequired()]
+    )
+    has_wifi = RadioField(
+        "Has Wifi?",
+        choices=[("yes","Yes"),("no", "No")],
+        validators=[InputRequired()]
+    )
+    can_take_calls = RadioField(
+        "Can calls be taken?",
+        choices=[("yes","Yes"),("no", "No")],
+        validators=[InputRequired()]
+    )
     seats = StringField("Number of Seats", validators=[DataRequired()])
     coffee_price = StringField("Coffee Price", validators=[DataRequired()])
     submit = SubmitField("Submit Cafe")
